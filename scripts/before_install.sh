87f57d08d0cb07d296d337833747b5975c0a6a36
aspdiscovery123 <48548207+aspdiscovery123@users.noreply.github.com>
2023-11-13 17:04:25 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,4 @@
-# remove or drop code deploy agent
-sudo rm -rf /home/ubuntu/install
 
-# installl code deploy agent
-
-sudo apt-get -y update
-sudo apt-get -y install ruby
-sudo apt-get -y install wget
-cd /home/ubuntu
-
-wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-
-sudo chmod +x ./install
-sudo ./install auto
 
 # update is and install python3
 
